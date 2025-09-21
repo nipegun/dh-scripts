63c8af49b593ea45e122385cd90c6b00bd9ffaea
NiPeGun <nipegun@gmail.com>
2025-09-21 18:45:06 +0200

    Update Metasploitable3-Windows-Preparar-ConQEMU.sh

--- Recursos/Software/Vulnerable/MVs/Metasploitable3-Windows-Preparar-ConQEMU.sh
+++ Recursos/Software/Vulnerable/MVs/Metasploitable3-Windows-Preparar-ConQEMU.sh
@@ -9,13 +9,13 @@
 # Script de NiPeGun para preparar la máquina de Metasploitable3-Windows usando QEMU en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/Vulnerable/MV/Metasploitable3-Windows-Preparar-ConQEMU.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/Vulnerable/MVs/Metasploitable3-Windows-Preparar-ConQEMU.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/Vulnerable/MV/Metasploitable3-Windows-Preparar-ConQEMU.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/Vulnerable/MVs/Metasploitable3-Windows-Preparar-ConQEMU.sh | sed 's-sudo--g' | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/Vulnerable/MV/Metasploitable3-Windows-Preparar-ConQEMU.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/Vulnerable/MVs/Metasploitable3-Windows-Preparar-ConQEMU.sh | nano -
 # ----------
 
 # Definir constantes de color
