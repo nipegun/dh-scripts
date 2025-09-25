dd610f9cd8356f884dd7b6485e606f810df96a3f
NiPeGun <nipegun@gmail.com>
2025-09-25 15:37:29 +0200

    Update EnHashes-Instalar-SHA256.sh

--- Recursos/WordLists/EnHashes-Instalar-SHA256.sh
+++ Recursos/WordLists/EnHashes-Instalar-SHA256.sh
@@ -63,11 +63,11 @@
   menu=(dialog --checklist "Marca las WordLists EnHashes que quieras instalar:" 22 80 16)
     opciones=(
       1 "WeakPass RockYou SHA256   (  1,1 GB descomprimido)" off
-      2 "WeakPass 4 Latin SHA256   (155,0 GB descomprimido)" off
-      3 "WeakPass 4 Merged SHA256  (260,0 GB descomprimido)" off
-      4 "WeakPass 4 Policy SHA256  ( 23,0 GB descomprimido)" off
-      5 "WeakPass 4a Latin SHA256  (590,0 GB descomprimido)" off
-      6 "WeakPass 4a Policy SHA256 (125,0 GB descomprimido)" off
+      2 "WeakPass 4 Latin SHA256   (164,1 GB descomprimido)" off
+      3 "WeakPass 4 Merged SHA256  (273,2 GB descomprimido)" off
+      4 "WeakPass 4 Policy SHA256  ( 24,5 GB descomprimido)" off
+      5 "WeakPass 4a Latin SHA256  (xxx,x GB descomprimido)" off
+      6 "WeakPass 4a Policy SHA256 (133,6 GB descomprimido)" off
     )
     choices=$("${menu[@]}" "${opciones[@]}" 2>&1 >/dev/tty)
 
