819da0a12cf3db713203becfd5e03e73da9e3216
NiPeGun <nipegun@gmail.com>
2025-10-14 21:03:46 +0200

    Update 00-InstalarSoftwareParaCLI.sh

--- Recursos/Software/ParaCLI/00-InstalarSoftwareParaCLI.sh
+++ Recursos/Software/ParaCLI/00-InstalarSoftwareParaCLI.sh
@@ -3,6 +3,9 @@
 # Actualizar lista de paquetes disponibles en los repositorios
   sudo apt-get -y update
 
+# Esteganografía
+  sudo apt-get -y install stegseek
+
 # Web
   sudo apt-get -y install nikto
   sudo apt-get -y install sqlmap
