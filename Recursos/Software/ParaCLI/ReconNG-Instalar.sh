d9e4ff15491ffe8edd764af3c1ae895ec1c221f2
NiPeGun <nipegun@gmail.com>
2025-10-22 04:20:32 +0200

    Update and rename ReconNG-Instalar1.sh to ReconNG-Instalar.sh

--- Recursos/Software/ParaCLI/ReconNG-Instalar.sh
+++ Recursos/Software/ParaCLI/ReconNG-Instalar.sh
@@ -71,9 +71,10 @@
           sudo apt-get -y install git
           echo ""
         fi
+      cd $HOME
+      rm -rf $HOME/HackingTools/OSINT/recon-ng
       mkdir -p $HOME/HackingTools/OSINT/ 2> /dev/null
       cd $HOME/HackingTools/OSINT/
-      rm -rf $HOME/HackingTools/OSINT/recon-ng/
       git clone https://github.com/lanmaster53/recon-ng.git
     # Instalar requisitos
       cd recon-ng
