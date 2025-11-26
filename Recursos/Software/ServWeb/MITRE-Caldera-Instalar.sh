58e91f4b62dfcb80afc66b2dc7bbf5e0323f1312
NiPeGun <nipegun@gmail.com>
2025-11-26 01:44:45 +0100

    Update and rename Caldera-Instalar.sh to MITRE-Caldera-Instalar.sh

--- Recursos/Software/ServWeb/MITRE-Caldera-Instalar.sh
+++ Recursos/Software/ServWeb/MITRE-Caldera-Instalar.sh
@@ -9,13 +9,13 @@
 # Script de NiPeGun para instalar y configurar Caldera en Debian
 #
 # Ejecución remota (puede requerir permisos sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/ServWeb/Caldera-Instalar.sh | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/ServWeb/MITRE-Caldera-Instalar.sh | bash
 #
 # Ejecución remota como root (para sistemas sin sudo):
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/ServWeb/Caldera-Instalar.sh | sed 's-sudo--g' | bash
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/ServWeb/MITRE-Caldera-Instalar.sh | sed 's-sudo--g' | bash
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/ServWeb/Caldera-Instalar.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/dh-scripts/refs/heads/main/Recursos/Software/ServWeb/MITRE-Caldera-Instalar.sh | nano -
 # ----------
 
 # Definir constantes de color
